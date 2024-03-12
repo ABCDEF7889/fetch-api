8994f25bac122cc162be78d2cb327eeae4711c2f
ABCDEF7889 <42933723+ABCDEF7889@users.noreply.github.com>
2024-03-12 19:47:34 +0530

    created wid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import {AppRoutingModule} from "./app-routing.module";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+ standalone: true,
+ imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private http:HttpClient){}
   title = 'fetch-api';
+li:any;
+lis=[];
+let prod:any;
+  ngOnInit(): void {
+    // console.log('hi');
+    this.http.get('https://dummyjson.com/products')
+    .subscribe(Response=>{
+      if(Response){
+        hideloader();
+      }
+      console.log(Response);
+      this.li=Response;
+      this.lis=this.title.prod;
+    });
+    function hideloader(){
+//      document.getElementById('loading').style.display='none';
+    }
+  }
 }
